0e3d08533168ad8819e74384ebf7b1ec748760c1
eliudnyamai <eliudmitau@gmail.com>
2025-02-18 21:49:53 +0300

    test

--- home/support/web/godspeedoffers.com/public_html/database/plugins/awesome/hello/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Client View Groups
-Route::group(['middleware' => ['web'], 'namespace' => '\{{ author_class }}\{{ name_class }}\Controllers'], function () {
-    Route::get('plugins/{{ author }}/{{ name }}', 'DashboardController@index');
-});
--- resources/views/emails/auth/verify_email.blade.php
+++
@@ -1,9 +0,0 @@
-@component('mail::message')
-{!! $content !!}
-@component('mail::button', ['url' => $url])
-{{ __('locale.auth.verify') }}
-@endcomponent
-
-{{ __('locale.labels.thanks') }},<br>
-{{ config('app.name') }}
-@endcomponent
