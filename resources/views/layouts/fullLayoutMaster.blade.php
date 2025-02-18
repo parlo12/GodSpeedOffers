adbea1e6ca9b5fdb10de6d9629b3f76aa30355d1
eliudnyamai <eliudmitau@gmail.com>
2025-02-19 00:23:14 +0300

    new

--- resources/views/layouts/fullLayoutMaster.blade.php
+++ resources/views/layouts/fullLayoutMaster.blade.php
@@ -19,7 +19,7 @@
     <title>@yield('title') - {{config('app.title')}}</title>
     <link rel="shortcut icon" type="image/x-icon" href="<?php echo asset(config('app.favicon')); ?>"/>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
-
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     {{-- Include core + vendor Styles --}}
     @include('panels/styles')
 
