68549a9ff1fafa1bfd24e6c67c79c5bb30205245
eliudnyamai <eliudmitau@gmail.com>
2025-03-20 01:51:33 +0300

    updating chat

--- resources/views/customer/ChatBox/_sidebarcopy.blade.php
+++ resources/views/customer/ChatBox/_sidebarcopy.blade.php
@@ -495,7 +495,7 @@
                 .then(data => {
                     if (data.status == 'success') {
                         console.log(data);
-                        location.reload()
+                       // location.reload()
                         // Toggle the star icon
                     } else {
                         console.error('Error removing fresh lead:', data.message);
