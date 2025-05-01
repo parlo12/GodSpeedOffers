580e3870e69382e550c379871afe303901884678
eliudnyamai <eliudmitau@gmail.com>
2025-05-01 12:21:57 +0300

    workflow message error fix

--- app/Http/Controllers/Customer/ChatBoxController.php
+++ app/Http/Controllers/Customer/ChatBoxController.php
@@ -971,7 +971,7 @@ class ChatBoxController extends Controller
     private function get_message($phoneNumber)
     {
         try {
-            $response = Http::get("https://internaltools.godspeedoffers.com/api/get-message/{$phoneNumber}");
+            $response = Http::get("https://internaltools.godspeedoffers.com/api/get-message/{$phoneNumber}&workflow_apikey=" . auth()->user()->workflow_apikey);
 
             // Check if the response status is OK
             if ($response->successful()) {
